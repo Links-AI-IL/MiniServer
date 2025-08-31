bb3d245a722322bc6ddaac0db43ff966c25bacd5
ShaniTouitouLinks <shani@links-workplace.com>
2025-08-31 11:38:48 +0300

    update the api

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
-ANTHROPIC_API_KEY = "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"
+ANTHROPIC_API_KEY = "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"
 ANTHROPIC_MODEL = "claude-3-5-sonnet-20240620"
 MAX_TOKENS = 400
 PORT = 5001
 DB_URL = "sqlite:///rag.db"
+
