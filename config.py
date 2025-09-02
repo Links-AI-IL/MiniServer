79ed794a6e806c9dc34ebbc67f76ac98325f8275
shneor <shneor@links-workplace.com>
2025-09-02 16:54:42 +0300

    Update max token to 2000

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 ANTHROPIC_API_KEY = os.environ.get("CLAUDE_API_KEY")
 ANTHROPIC_MODEL = "claude-3-5-sonnet-20240620"
-MAX_TOKENS = 400
+MAX_TOKENS = 2000
 PORT = 5001
 DB_URL = "sqlite:///rag.db"
 
